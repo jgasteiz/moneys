9a5bc5608f68d6f4cee6233e5fcfacd9c2b24075
Javi Manzano <javi.manzano.oller@gmail.com>
2014-10-23 22:21:13 +0100

    Add paginateBy to the query and base for drawing charts

--- static/js/views.js
+++ static/js/views.js
@@ -12,6 +12,7 @@
         },
         template: _.template($('#tpl-expense-collection').html()),
         page: 1,
+        paginateBy: 20,
         
         initialize: function() {
             this.listenTo(this.collection, 'reset add change remove', this.render, this);
@@ -28,12 +29,41 @@
         fetchCollection: function() {
             this.collection.fetch({
                 data: {
-                    page: this.page
+                    page: this.page,
+                    paginate_by: this.paginateBy
+                }
+            });
+        },
+        generateChart: function(data) {
+            c3.generate({
+                bindto: this.el,
+                data: {
+                    columns: [
+                        data
+                    ],
+                    type: 'bar'
+                },
+                bar: {
+                    width: {
+                        ratio: 0.5
+                    }
                 }
             });
         },
         render: function() {
             this.page = this.collection.page;
+
+            var expenses = _.map(this.collection.models, function(expense) {
+                if (expense.has('debit_amount')) {
+                    return parseFloat(expense.get('debit_amount'));
+                } else {
+                    return 0;
+                }
+            });
+
+            // TODO: uncomment when generateChart makes sense.
+            // this.generateChart(['Debit expenses'].concat(expenses));
+
             this.$el.html(this.template({
                 page: this.collection.page,
                 hasNextPage: this.collection.hasNextPage,
